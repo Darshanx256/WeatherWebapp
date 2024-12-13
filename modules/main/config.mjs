cdef3eb2a5931d0d5edcbb853eb133bffb039197
Darshanx256 <darshanp7666@gmail.com>
2024-12-13 15:38:04 +0000

    Deploying to gh-pages from @ Darshanx256/WeatherWebapp@86d3a7a658b79902eb4be320bda8372b92c6622f 🚀

--- modules/main/config.mjs
+++ modules/main/config.mjs
@@ -1,4 +1,3 @@
 // config.mjs
-console.log(import.meta.env);  // This will print out all the environment variables
 export const OPENWEATHERMAP_KEY = import.meta.env.VITE_OPENWEATHERMAP;
 export const GEOAPIFY_KEY = import.meta.env.VITE_GEOAPIFY;
